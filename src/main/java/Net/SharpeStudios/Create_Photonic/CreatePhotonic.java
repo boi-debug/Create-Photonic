caaa68202f972e0115351c3a90c6f5e59ef64f21
DevBoi <Jakesharpe79@gmail.com>
2025-03-16 03:44:44 +0000

    Added

--- src/main/java/Net/SharpeStudios/Create_Photonic/CreatePhotonic.java
+++ src/main/java/Net/SharpeStudios/Create_Photonic/CreatePhotonic.java
@@ -1,7 +1,9 @@
 package Net.SharpeStudios.Create_Photonic;
 
 import Net.SharpeStudios.Create_Photonic.Block.ModBlocks;
+import Net.SharpeStudios.Create_Photonic.Items.ModCreativeModeTabs;
 import Net.SharpeStudios.Create_Photonic.Items.ModItems;
+import net.minecraft.world.item.ItemStack;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -31,6 +33,8 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Supplier;
+
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreatePhotonic.MOD_ID)
 public class CreatePhotonic
@@ -41,7 +45,7 @@ public class CreatePhotonic
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MOD_ID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
+
 
 
 
@@ -50,13 +54,6 @@ public class CreatePhotonic
             .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("CreatePhotonic")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -69,8 +66,8 @@ public class CreatePhotonic
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
+        ModCreativeModeTabs.Register(modEventBus);
         // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
---
+++ src/main/java/Net/SharpeStudios/Create_Photonic/Items/ModCreativeModeTabs.java
@@ -0,0 +1,29 @@
+package Net.SharpeStudios.Create_Photonic.Items;
+
+import Net.SharpeStudios.Create_Photonic.Block.ModBlocks;
+import Net.SharpeStudios.Create_Photonic.CreatePhotonic;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> Creative_Mode_Tab =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreatePhotonic.MOD_ID);
+
+    public static void Register(IEventBus eventBus){
+        Creative_Mode_Tab.register(eventBus);
+    }
+    public static final Supplier<CreativeModeTab> Create_Photonic_Tab = Creative_Mode_Tab.register("Create_Photonic_Tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.Beamer.get()))
+                    .title(Component.translatable("creativetab.createphotonic.CreatePhotonic"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.Sword);
+                        output.accept(ModItems.Beamer);
+                        output.accept(ModBlocks.Beamer_Block);
+                    }).build());
+}
